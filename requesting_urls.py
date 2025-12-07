882d583efa6462417fe4ebbf7b7bf4f40c6433c7
veblei <v.leihne@gmail.com>
2025-12-07 16:35:45 +0100

    Removed output and params, added headers that specify user agent as per wikipedia robots.txt

--- requesting_urls.py
+++ requesting_urls.py
@@ -3,33 +3,24 @@ from typing import Dict, Optional
 import requests
 
 
-def get_html(url: str, params: Optional[Dict] = None, output: Optional[str] = None):
+def get_html(url: str):
     """Get an HTML page and return its contents.
 
     Args:
         url (str):
             The URL to retrieve.
-        params (dict, optional):
-            URL parameters to add.
-        output (str, optional):
-            (optional) path where output should be saved.
     Returns:
         html (str):
             The HTML of the page, as text.
     """
+    headers = {"User-Agent": "NBA-Statistics-Crawler/1.0"}
     # passing the optional parameters argument to the get function
     response = None
-    if (params):
-        response = requests.get(url, params=params)
+    if (headers):
+        response = requests.get(url, headers=headers)
     else:
         response = requests.get(url)
 
     html_str = response.text
-
-    if output:
-        # if output is specified, the response txt and url get printed to a
-        # txt file with the name in `output`
-        with open (output, "w") as f:
-            f.write(f"${url}\n${html_str}")
 
     return html_str
