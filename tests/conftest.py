5bff8ad204a74e2ef50292ed52cf0c51fe7763fc
veblei <v.leihne@gmail.com>
2025-12-07 16:50:09 +0100

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import sys
 from pathlib import Path
 
-assignment4 = Path(__file__).parent.parent.absolute()
+my_dir = Path(__file__).parent.parent.absolute()
 
-# Ensure assignment4 dir is on sys.path
-sys.path.insert(0, str(assignment4))
+# Ensure this dir is on sys.path
+sys.path.insert(0, str(my_dir))
