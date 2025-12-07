7bb215c0788d1580f75d19b16ef288984692aa3f
veblei <v.leihne@gmail.com>
2025-12-07 16:50:17 +0100

    Update test_requesting_urls.py

--- tests/test_requesting_urls.py
+++ tests/test_requesting_urls.py
@@ -18,34 +18,3 @@ def test_get_html_no_params(url, expected):
     assert "<!DOCTYPE" in html
     assert "<html" in html
     assert expected in html
-
-
-# Test with params
-@pytest.mark.parametrize(
-    "params, expected",
-    [
-        ({"title": "Main_Page"}, "<title>Wikipedia"),
-        ({"title": "Hurricane_Gonzalo", "oldid": "983056166"}, "<title>Hurricane"),
-    ],
-)
-def test_get_html_params(params, expected):
-    url = "https://en.wikipedia.org/w/index.php"
-
-    html = get_html(url, params=params)
-    assert expected in html
-
-
-def test_get_html_output(tmpdir):
-    url = "https://uio-in3110.github.io"
-    dest = tmpdir.join("output.txt")
-    html = get_html(url, output=str(dest))
-    assert dest.exists()  # assert output file was created
-
-    with dest.open() as f:
-        written_output = f.read()
-
-    first_line, rest = written_output.split("\n", 1)
-    assert url in first_line
-    assert "<html" in rest
-    assert "Higher Level Programming" in rest
-    assert rest.strip().endswith("</html>")
